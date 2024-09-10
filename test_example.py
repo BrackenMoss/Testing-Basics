434c87c8e448558b702bac24c0cda137562cb6ed
Brayden Connole <brayden.connole@gmail.com>
2024-09-10 11:22:15 -0600

    fix test

--- test_example.py
+++ test_example.py
@@ -24,4 +24,4 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzz(10), [1,2, 'Fizz', 4, 'Buzz', 'Fizz', 7,8,'Fizz', 'Buzz'])
 
     def test_fifteen(self):
-        self.assertEqual(fizzbuzz(10), [1,2, 'Fizz', 4, 'Buzz', 'Fizz', 7,8,'Fizz', 'Buzz', 11,'Fizz',13,14,'FizzBuzz'])
+        self.assertEqual(fizzbuzz(15), [1,2, 'Fizz', 4, 'Buzz', 'Fizz', 7,8,'Fizz', 'Buzz', 11,'Fizz',13,14,'FizzBuzz'])
