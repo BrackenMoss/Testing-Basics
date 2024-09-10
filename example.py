f697ab4fbbc020ff967678846f143060f2aee4bd
Brayden Connole <brayden.connole@gmail.com>
2024-09-10 10:57:00 -0600

    create fizzbuzz function

---
+++ example.py
@@ -0,0 +1,9 @@
+# FizzBuzz
+# Write a program that returns the numbers from 1 to 100 in a list. 
+# For multiples of 3, return "Fizz" instead of the number, and for multiples of 5, return "Buzz."
+# For numbers that are multiples of both 3 and 5, return "FizzBuzz."
+
+
+def fizzbuzz(input_number):
+    return [1]
+
