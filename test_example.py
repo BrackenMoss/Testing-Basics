1d746f809a57e509c3e154ac18baecf553743c6e
Brayden Connole <brayden.connole@gmail.com>
2024-09-10 11:06:53 -0600

    add test 3

--- test_example.py
+++ test_example.py
@@ -13,3 +13,6 @@ class TestFizzBuzz(unittest.TestCase):
 
     def test_two(self):
         self.assertEqual(fizzbuzz(2), [1,2])
+
+    def test_three(self):
+        self.assertEqual(fizzbuzz(3), [1,2, 'Fizz'])
